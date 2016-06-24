1f6fc8cfe9362b051d8f83f35e7ee0ab592beb28
Fernando Barillas <fbis251@mailbox.org>
2016-06-24 01:27:26 -0700

    rest.d: Use UsersDb code, cleanup

--- source/linkservice/rest.d
+++ source/linkservice/rest.d
@@ -35,51 +35,75 @@ interface Api {
 ///
 class LinkServiceRestApi : Api {
 
+    User getUserFromAuthToken(string authToken) {
+        User user = usersDb.getUserFromAuthToken(authToken);
+        if(!isUserIdValid(user)) {
+            throw new HTTPStatusException(HTTPStatus.unauthorized, "Please log in");
+        }
+        return user;
+    }
+
 override:
     Json postAdd(string _authToken, Link link) {
-        checkAuthToken(_authToken);
+        logInfo("POST /add");
+        debugfln("postAdd() link.title = %s, link.url = %s", link.title, link.url);
         enforce(validateUrl(link.url), "Invalid URL");
+
+        User user = getUserFromAuthToken(_authToken);
         AddLinkResponse response;
-        logInfo("Trying to add URL: %s", link.url);
-        Link responseLink = addLinkToDatabase(userId, link);
+
+        debugfln("Trying to add URL: %s", link.url);
+        Link responseLink = addLinkToDatabase(user.userId, link);
         response.successful = isLinkIdValid(responseLink);
         response.link = responseLink;
         return serializeToJson(response);
     }
 
     Json postLogin(string username, string password) {
-        LoginResponse response;
-        response.successful = checkPostLogin(username, password);
-        if(response.successful) {
-            response.refreshToken = getUserRefreshToken(username);
-            response.username = username;
+        logInfo("POST /login");
+        debugfln("postLogin() username = %s", username);
+
+        User user = usersDb.getUser(username);
+        if(!validateLogin(user, password)) {
+            throw new HTTPStatusException(HTTPStatus.unauthorized, "Invalid username or password");
         }
 
-        return serializeToJson(response);
-    }
+        LoginResponse response;
+        response.successful = true;
+        response.authToken = user.authToken;
+        response.username = user.username;
 
-    Json getDelete(string _authToken, long linkId) {
-        checkAuthToken(_authToken);
-        logInfo("Trying to delete ID: %d", linkId);
-        SuccessResponse response;
-        response.successful = deleteUrlFromDatabase(userId, linkId);
         return serializeToJson(response);
     }
 
     Json getArchive(string _authToken, long linkId, bool isArchived) {
-        checkAuthToken(_authToken);
-        logInfo("Trying to archive ID: %d", linkId);
+        logInfo("GET /archived/%d?isArchived=%d", linkId, isArchived);
+        debugfln("getDelete() linkId = %d, isArchived = %d", linkId, isArchived);
+
+        User user = getUserFromAuthToken(_authToken);
         SuccessResponse response;
         response.successful = false;
         // TODO: Allow setting link.isArchived = isArchived
         return serializeToJson(response);
     }
 
+    Json getDelete(string _authToken, long linkId) {
+        logInfo("GET /delete/%d", linkId);
+        debugfln("getDelete() linkId = %d", linkId);
+
+        User user = getUserFromAuthToken(_authToken);
+        SuccessResponse response;
+        response.successful = deleteLinkFromDatabase(user.userId, linkId);
+        return serializeToJson(response);
+    }
+
     Json getList(string _authToken) {
-        logInfo("getList()");
-        checkAuthToken(_authToken);
-        linksList = getLinksFromDatabase(userId);
-        logInfo("URLs: %d", linksList.linksList.length);
+        logInfo("GET /list");
+
+        User user = getUserFromAuthToken(_authToken);
+
+        LinksList linksList = getLinksFromDatabase(user.userId);
+        logInfo("User:%s, URL count: %d", user.username, linksList.linksList.length);
 
         return serializeToJson(linksList);
     }
