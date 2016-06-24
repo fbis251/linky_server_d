4966d269b040a1efb8a1754a34f353d55aa4cfb1
Fernando Barillas <fbis251@mailbox.org>
2016-06-24 01:11:51 -0700

    crypto.d: Cleanup

--- source/linkservice/utils/crypto.d
+++ source/linkservice/utils/crypto.d
@@ -6,47 +6,36 @@ import botan.passhash.bcrypt;
 import botan.rng.rng;
 import botan.rng.auto_rng;
 
-///
-const string PASSWORD_FILE = "private/password.txt";
 /// BCrypt Iterations
 const int WORK_FACTOR = 10;
 
 ///
-bool checkBcryptPassword(const string password) {
-    return checkBcrypt(password, readHashFile());
+bool checkBcryptPassword(const string password, const string storedHash) {
+    return checkBcrypt(password, storedHash);
 }
 
 string generateHash(const string plaintext) {
     // bcrypt has no support for NUL characters and passwords > 72 characters
     // Generate a sha384 hash of the plaintext and pass that to bcrypt to mitigate this
-    auto hash = cast(string) plaintext.sha384Of;
+    //auto hash = cast(string) plaintext.sha384Of;
+    auto hash = getSha384HexString(plaintext);
     Unique!AutoSeededRNG rng = new AutoSeededRNG;
     return generateBcrypt(hash, *rng, WORK_FACTOR);
 }
 
-///
-string getRefreshToken() {
-    // TODO: Improve this
-    return format("%-(%02x%)", readHashFile().sha256Of);
+/// Generates 16 random bytes and returns the sha256 digest of them as a hex string
+string generateRefreshToken() {
+    Unique!AutoSeededRNG rng = new AutoSeededRNG;
+    ubyte[16] randomBytes;
+    rng.randomize(randomBytes.ptr, randomBytes.length);
+    writeln(getSha256HexString(randomBytes));
+    return getSha256HexString(randomBytes);
 }
 
-///
-string readHashFile() {
-    string result = "";
-
-    auto lines = File(PASSWORD_FILE)   // Open for reading
-        .byLineCopy()      // Read persistent lines
-        .array();           // into an array
-    if(lines.length > 0) {
-        result = lines[0];
-    }
-
-    return result;
+string getSha256HexString(ubyte[] array) {
+    return toHexString(sha256Of(array).dup);
 }
 
-///
-void writeHashFile(string hash) {
-    auto f = File(PASSWORD_FILE, "w"); // open for appending
-    f.write(hash);
-    f.close();
+string getSha384HexString(string inputString) {
+    return toHexString(sha384Of(inputString).dup);
 }
