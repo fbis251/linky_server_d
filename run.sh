41e7db9c0fa1e528a4400c18be139f944c423621
Fernando Barillas <fbis251@mailbox.org>
2016-09-19 17:58:10 -0700

    run.sh: Simplify when-changed command

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -x
 NAME="link_saver"
-DUB_BUILD="time dub build --compiler=ldc"
+DUB_BUILD="time dub build"
 DEBUG_LEVEL=""
 
 clear;
 ./bin/$NAME $DEBUG_LEVEL &
-when-changed ./source/*/*.d ./source/*/*/*.d ./source/*.d ./views/*.dt -c "clear; $DUB_BUILD && ( killall $NAME; clear; ./bin/$NAME $DEBUG_LEVEL ) &"
+when-changed -r ./source ./views -1 -c "clear; $DUB_BUILD && ( killall $NAME; clear; ./bin/$NAME $DEBUG_LEVEL ) &"
